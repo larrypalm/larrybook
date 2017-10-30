69cb19b2ce9f3f91a4e5f14d0b87965263e7ee03
larrypalm <palm.gustav@gmail.com>
2017-10-30 15:57:36 +0100

    auth-branch added to test loggin functionality

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,19 +1,5 @@
 import firebase from '../firebase';
 
-//Sync db to state
-// export function fetchAllUsers(){
-//   return function(dispatch){
-//     firebase.database().ref("users")
-//     .on("value", snapshot => {
-//       let tempArray = []
-//       snapshot.forEach(child => {
-//         tempArray.push({...child.val(), key: child.key});
-//       })
-//       dispatch({type: "FETCH_ALL_USERS", users: tempArray})
-//     })
-//   }
-// }
-
 export function addUserListener(){
   return function(dispatch){
     return firebase.database().ref("users")
@@ -86,30 +72,6 @@ export function addMovies() {
       .catch(error => dispatch({type: "FETCH_ERROR", error}))
   }
 }
-
-//call to api
-// export function postUser(user) {
-//   return function(dispatch){
-//      fetch('https://fend-api.herokuapp.com/notes', {
-//        method: 'POST',
-//        headers: {
-//          'Accept': 'application/json',
-//          'Content-Type': 'application/json',
-//        },
-//        body: JSON.stringify(user)
-//      })
-//      .then(response => response.json())
-//      .then(json => dispatch(addUser(user)))
-//      .catch(error => console.log(error));
-//   }
-// }
-
-// firebase.database().ref('users')
-//   .push({text: "Learn firebase", completed: false})
-//   firebase.database().ref('users')
-//   .on("value", users => {
-//     console.log(users.val());
-//   })
 
 export function addPost(post) {
   return {
